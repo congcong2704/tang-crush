8939bfef8c67f29da6ae930a96c8132751f5d6d8
congcong2704 <117121445+congcong2704@users.noreply.github.com>
2022-11-02 10:56:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
 const CONFIG = {
     introTitle: ' Nhung à!',
-    introDesc: ` em có biết anh iu em nhiều lắm ?
-    Em như là định mệnh của đời anh vậy
-    Đằng sau chữ yêu đây là thương!  <33`,
+    introDesc: ` tối đi ăn không ?
+    <33`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: ' 😙',
+    desc: ' ',
+    btnYes: 'Điiiii <33',
+    btnNo: 'Không :3',
+    question:'đi ăn nha <3',
+    btnReply: 'Gửi cho t <3',
+    reply: 'tối tau rảnh đi ăn với tau luôn <33333333',
+    mess: ' 🥰. ok nè 😘😘',
+    messDesc: 'Tối nay 7h.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/trieu.nhat.9883739'
+    messLink: 'https://www.facebook.com/profile.php?id=100018505864932'
 }
